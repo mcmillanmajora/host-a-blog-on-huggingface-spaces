8492b3593d75017325de74b9bf3cb759b53cfe0c
mcmillanmajora <mcmillan.majora@gmail.com>
2022-01-26 18:11:51 -0800

    Update and rename dataset_exploration.md to dataset_exploration.py

---
+++ posts/dataset_exploration.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+title = "Dataset Exploration"
+description = "Comparison of hate speech detection datasets"
+date = "2022-01-26"
+thumbnail = "images/huggingface_logo.png"
+
+def run_article():
+  st.markdown("""
+  # Making a Hate Speech Dataset
+
+  This is where labels and design choices will go.
+
+  # Dataset Measurements Tool
+
+  For now, here's a link to the [space](https://huggingface.co/spaces/huggingface/data-measurements-tool).
+  """)
