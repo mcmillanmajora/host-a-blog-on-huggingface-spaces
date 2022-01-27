5d1a161a454e60a7e7d36593d972b8a7a738ac29
mcmillanmajora <mcmillan.majora@gmail.com>
2022-01-26 18:14:48 -0800

    Update and rename conclusion.md to conclusion.py

---
+++ posts/conclusion.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+title = "Key Takeaways"
+description = "The key takeaways from this exploration"
+date = "2022-01-26"
+thumbnail = "images/huggingface_logo.png"
+
+def run_article():
+  st.markdown("""
+  # Conclusion
+
+  Here are some of the main ideas we have conveyed in this exploration: 
+  - Defining hate speech is hard and changes depending on your context and goals.
+  - Capturing a snapshot of what you've defined to be hate speech in a dataset is hard.
+  - Models learn lots of different things based on the data it sees, and that can include things you didn't intend for them to learn.
+
+  Action items?
+  """)
