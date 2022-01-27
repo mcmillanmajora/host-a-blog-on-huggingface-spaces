9faa18f432e4db5f57a875ae29ac4f786e8d187f
mcmillanmajora <mcmillan.majora@gmail.com>
2022-01-26 18:08:28 -0800

    Update and rename welcome.md to welcome.py

---
+++ posts/welcome.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+title = "Welcome Page"
+description = "Introduction"
+date = "2022-01-26"
+thumbnail = "images/huggingface_logo.png"
+
+def run_article():
+  st.markdown("""
+  # Welcome!
+
+  Welcome to the Task Exploration Activity for hate speech detection! In this series of modules, you'll learn about the history of hate speech detection as a task in   the larger pipeline of automatic content moderation (ACM). You'll also be able to interact with and compare datasets and models built for this task. 
+
+  The goal of this exploration is to share the design considerations and challenges faced when using algorithms to detect hate speech.
+  """)
